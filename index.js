c521b2ae6ea9c17494eddf183777ed3be0301929
mrodcir <114927916+mrodcir@users.noreply.github.com>
2023-02-22 20:46:07 +0100

    3 files modified

--- index.js
+++ index.js
@@ -2,12 +2,31 @@
 import './style.css';
 
 //Gráfico de columnas. Mi primer gráfico
-var visualization = d3plus.viz()
-.container("#viz1")
-.data([{"year": 1991, "name":"alpha", "value": 15},
-{"year": 1992, "name":"alpha", "value": 20},])
-.type("bar")
-.id("name")
-.x("year")
-.y("value")
-.draw()
+var visualization = d3plus
+  .viz()
+  .container("#viz1")
+  .data([
+    {"year": 1991, "name":"alpha", "value": 15},
+    {"year": 1992, "name":"alpha", "value": 20},
+    ])
+  .type("bar")
+  .id("name")
+  .x("year")
+  .y("value")
+  .draw();
+
+  //Gráfico de multicolumnas. Mi segundo gráfico
+d3.json(
+  "https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", 
+  function(data) {
+    var visualization = d3plus
+     .viz()
+     .container("#viz2")
+     .data(data)
+     .type('bar')
+     .id('name')
+     .x('year')
+     .y('value')
+     .axes({ ticks: 'false' })
+     .draw();
+      });
